f41a07410477ee2ac2b7717e2c4a59f30a4c265a
C0deWave <jamg123123@naver.com>
2022-10-31 08:14:50 +0900

    Feat: eks_cluster_module v1.0.0

---
+++ eks_cluster/input.tf
@@ -0,0 +1,8 @@
+variable "cluster_name" {}
+variable "eks_role_arn" {}
+variable "eks_cluster_master_subnet" {}
+variable "eks_cluster_nodegroup_subnet" {}
+variable "instance_max_node_size" {}
+variable "instance_disired_node_size" {}
+variable "instance_min_node_size" {}
+variable "instance_type_list" {}
