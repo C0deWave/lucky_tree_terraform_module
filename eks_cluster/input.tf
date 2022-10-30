98869f8bf4126b01c074a8e01057f6978b150579
C0deWave <jamg123123@naver.com>
2022-10-31 08:28:18 +0900

    Fix: list(string) 타입의 경우 명시

--- eks_cluster/input.tf
+++ eks_cluster/input.tf
@@ -1,8 +1,14 @@
 variable "cluster_name" {}
 variable "eks_role_arn" {}
-variable "eks_cluster_master_subnet" {}
-variable "eks_cluster_nodegroup_subnet" {}
+variable "eks_cluster_master_subnet" {
+    type=list(string)
+}
+variable "eks_cluster_nodegroup_subnet" {
+    type = list(string)
+}
 variable "instance_max_node_size" {}
 variable "instance_disired_node_size" {}
 variable "instance_min_node_size" {}
-variable "instance_type_list" {}
+variable "instance_type_list" {
+    type=list(string)
+}
