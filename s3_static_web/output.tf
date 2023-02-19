982d8a68d901212d631bccc4a4c9bfcb787d3a92
jmjang <jamg123123@naver.com>
2023-02-19 14:19:16 +0900

    Fix: add output domain name

--- s3_static_web/output.tf
+++ s3_static_web/output.tf
@@ -7,3 +7,6 @@ output "cloudfront_aliases" {
 output "cloudfront_id" {
   value = aws_cloudfront_distribution.s3_distribution.id
 }
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
